f2ddc4cbae05320fce4d5548db4841f6736ee929
Mihkel Märtin <mihkelmartin@gmail.com>
2017-12-30 21:05:53 +0200

    Initial Commit

---
+++ Musician´s DiaryTests/Musician_s_DiaryTests.swift
@@ -0,0 +1,36 @@
+//
+//  Musician_s_DiaryTests.swift
+//  Musician´s DiaryTests
+//
+//  Created by Mihkel Märtin on 30/12/2017.
+//  Copyright © 2017 Mihkel Märtin. All rights reserved.
+//
+
+import XCTest
+@testable import Musician_s_Diary
+
+class Musician_s_DiaryTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
