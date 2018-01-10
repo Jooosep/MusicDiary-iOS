dc0e9b5a2fc1a3c9e90842d9caba32813a0fa4eb
Joosep Teemaa <joosep@Jooseps-iMac.local>
2018-01-10 04:47:52 -0800

    sqlite.swift library listed project

---
+++ Musician´s Diary/DBManager.m
@@ -0,0 +1,15 @@
+//
+//  DBManager.m
+//  Musician´s Diary
+//
+//  Created by Joosep Teemaa on 10/01/2018.
+//  Copyright © 2018 Mihkel Märtin. All rights reserved.
+//
+
+#import "DBManager.h"
+
+
+@implementation DBManager
+
+@end
+
--- Musician´s Diary/Teos.swift
+++ Musician´s Diary/Teos.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import UIKit
 
 /**
  * Created by mihkel on 2.05.2016.
